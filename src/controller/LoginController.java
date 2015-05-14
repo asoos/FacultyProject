50a6025512239b936eed345efe0e2e0edeabdfc7
Raul.Bob <raul.bob@endava.com>
2015-05-14 14:58:35 +0300

    going up to date

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,8 +1,34 @@
 package controller;
 
-public class LoginController {
+public class LoginController implements ILoginController{
+
 	
-	public LoginController(){
-		
+	
+	@Override
+	public int login(String username, String password, int type) {
+		// TODO Auto-generated method stub
+		if (type == 0){
+			//student
+		} else if (type == 1) {
+			//teacher
+		} else if (type == 2){
+			//admin
+		}
+		return 0;
 	}
+
+	@Override
+	public int register(String username, String password, int type) {
+		// TODO Auto-generated method stub
+		if (type == 0){
+			//student
+		} else if (type == 1) {
+			//teacher
+		} else if (type == 2){
+			//admin
+		}
+		return 0;
+	}
+	
+	
 }
