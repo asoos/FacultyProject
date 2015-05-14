238b643296b134d827c032468c93877e4f2c814c
Adrian Vas <Adrian.Vas@endava.com>
2015-05-14 14:07:55 +0300

    fa

--- src/controller/ROController.java
+++ src/controller/ROController.java
@@ -1,5 +1,48 @@
 package controller;
 
-public class ROController {
+public class ROController implements IROController {
+
+	@Override
+	public String getStudentCoursesById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTeacherCoursesById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNameById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSurnameById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getGenderById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getFathersNameById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getFacultiesById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 
 }
